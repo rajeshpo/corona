de9fe6165bd07433a46b3803c8dce8c061d628a7
C Rajesh <rajeshchitikala888@gmail.com>
2020-05-21 16:56:41 +0530

    small changes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 
 ReactDOM.render(<App />,document.getElementById('root'));
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
+
+ 
