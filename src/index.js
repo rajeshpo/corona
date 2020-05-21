525728a627a58d18437538a222886e4026442caf
C Rajesh <rajeshchitikala888@gmail.com>
2020-05-21 15:46:57 +0530

    added procfile

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 
 ReactDOM.render(<App />,document.getElementById('root'));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
